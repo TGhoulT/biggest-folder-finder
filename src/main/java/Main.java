feffc37b2c01eeacbaf247be441dd26b14351469
TGhoulT <mgenius012975@gmail.com>
2024-05-03 15:32:09 +0300

    Last commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,34 +1,28 @@
 import java.io.File;
-import java.util.Set;
 import java.util.concurrent.ForkJoinPool;
 
-public class Main {
-    public static void main(String[] args) {
-        String folderPath = "E:/";
+public class Main
+{
+    public static void main(String[] args)
+    {
+        ParametersBag bag = new ParametersBag(args);
+
+        String folderPath = bag.getPath();
+        long sizeLimit = bag.getLimit();
+
         File file = new File(folderPath);
+        Node root = new Node(file, sizeLimit);
 
         long start = System.currentTimeMillis();
 
-        FolderSizeCalculator calculator = new FolderSizeCalculator(file);
+        FolderSizeCalculator calculator =
+                new FolderSizeCalculator(root);
         ForkJoinPool pool = new ForkJoinPool();
-        long size = pool.invoke(calculator);
-        System.out.println(size);
+        pool.invoke(calculator);
 
-        /*System.out.println(getFolderSize(file));*/
+        System.out.println(root);
 
-        long duration = (System.currentTimeMillis() - start) / 1000;
-        System.out.println(duration + " seconds");
-
-    }
-    public static long getFolderSize(File folder) {
-        if (folder.isFile()) {
-            return folder.length();
-        }
-        long sum = 0;
-        File[] files = folder.listFiles();
-        for (File file : files) {
-            sum += getFolderSize(file);
-        }
-        return sum;
+        long duration = System.currentTimeMillis() - start;
+        System.out.println(duration + " ms");
     }
 }
